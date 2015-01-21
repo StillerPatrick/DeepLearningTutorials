de4ae0cf2c023752aa95388a6bb05866d68a9150
Frederic <nouiz@nouiz.org>
2015-01-21 14:35:00 -0500

    Try new hyper-parameter that give better result.

--- code/lstm.py
+++ code/lstm.py
@@ -390,9 +390,9 @@ def train_lstm(
     optimizer=adadelta,  # sgd, adadelta and rmsprop available, sgd very hard to use, not recommanded (probably need momentum and decaying learning rate).
     encoder='lstm',  # TODO: can be removed must be lstm.
     saveto='lstm_model.npz',  # The best model will be saved there
-    validFreq=370,  # Compute the validation error after this number of update.
-    saveFreq=1110,  # Save the parameters after every saveFreq updates
-    maxlen=100,  # Sequence longer then this get ignored
+    validFreq=-1,  # Compute the validation error after this number of update.
+    saveFreq=-1,  # Save the parameters after every saveFreq updates
+    maxlen=600,  # Sequence longer then this get ignored
     batch_size=16,  # The batch size during training.
     valid_batch_size=64,  # The batch size used for validation/test set.
     dataset='imdb',
@@ -586,6 +586,5 @@ if __name__ == '__main__':
     # See function train for all possible parameter and there definition.
     train_lstm(
         #reload_model="lstm_model.npz",
-        max_epochs=100,
-        test_size=500,
+        max_epochs=1000,
     )
